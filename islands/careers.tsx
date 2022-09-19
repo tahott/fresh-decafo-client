dc4c6ff1770b006505e80a0492aaac8c278b2d94
tahott <tahott@gmail.com>
2022-09-19 23:55:06 +0900

    feat: Add date input, add button

--- islands/careers.tsx
+++ islands/careers.tsx
@@ -67,39 +67,77 @@ export default function Careers(props: CareersProps) {
       <dialog
         ref={ref}
         class={tw
-          `bg-transparent p-0 m-0 pt-[50%] max-w-full w-full max-h-full ${slideBottom} ${backdrop}`}
+          `bg-transparent p-0 m-0 pt-[30%] max-w-full w-full max-h-full ${slideBottom} ${backdrop}`}
         onClick={onDialogClick}
       >
-        <div class={tw`bg-gray-50 max-w-3xl w-full h-full mx-auto divide-y divide-rose-900 rounded-t-lg p-2`}>
-          <div class={tw`flex justify-between`}>
-            <h2 class={tw`text-lg font-medium text-gray-900`}>Regist Career</h2>
-            <button
-              class={tw`py-1`}
-              onClick={(e) => {
-                (e.target as HTMLButtonElement).closest("dialog")!.close();
-              }}
-            >
-              <svg
-                class={tw`w-6 h-6 fill-current text-gray-600`}
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
-              </svg>
-            </button>
-          </div>
-          <div class={tw`p-2`}>
-            <div class={tw`mt-1`}>
-              <label class={tw`inline-block w-[100px]`} id='name'>company</label>
-              <input class={tw`ml-2 border`} for='name' />
-            </div>
-            <div class={tw`mt-1`}>
-              <label class={tw`inline-block w-[100px]`} id='job'>job</label>
-              <input class={tw`ml-2 border`} for='job' />
-            </div>
-          </div>
-        </div>
+        <CareerDialog />
       </dialog>
     </>
+  )
+}
+
+function CareerDialog() {
+  const [company, setCompany] = useState('');
+  const [job, setJob] = useState('');
+  const [inDate, setInDate] = useState('');
+  const [outDate, setOutDate] = useState('');
+
+  const handleCompany = (e: Event) => {
+    setCompany((e.currentTarget as HTMLInputElement).value)
+  }
+
+  const handleJob = (e: Event) => {
+    setJob((e.currentTarget as HTMLInputElement).value)
+  }
+
+  const handleInDate = (e: Event) => {
+    setInDate((e.currentTarget as HTMLInputElement).value)
+  }
+
+  const handleOutDate = (e: Event) => {
+    setOutDate((e.currentTarget as HTMLInputElement).value)
+  }
+
+  const handleAddCareer = () => {
+    console.log(company, job, inDate, outDate);
+  }
+
+  return (
+    <div class={tw`bg-gray-50 max-w-3xl w-full h-full mx-auto divide-y divide-rose-900 rounded-t-lg p-2`}>
+      <div class={tw`flex justify-between`}>
+        <h2 class={tw`text-lg font-medium text-gray-900`}>Regist Career</h2>
+        <button
+          class={tw`py-1`}
+          onClick={(e) => {
+            (e.target as HTMLButtonElement).closest("dialog")!.close();
+          }}
+        >
+          <svg
+            class={tw`w-6 h-6 fill-current text-gray-600`}
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
+          </svg>
+        </button>
+      </div>
+      <div class={tw`p-2`}>
+        <div class={tw`mt-1`}>
+          <label class={tw`inline-block w-[100px]`} id='company'>company</label>
+          <input class={tw`ml-2 border`} for='company' value={company} onInput={handleCompany} />
+        </div>
+        <div class={tw`mt-1`}>
+          <label class={tw`inline-block w-[100px]`} id='job'>job</label>
+          <input class={tw`ml-2 border`} for='job' value={job} onInput={handleJob} />
+        </div>
+        <div class={tw`mt-1`}>
+          <label class={tw`inline-block w-[100px]`} id='date'>date</label>
+          <input class={tw`ml-2 border`} for='date' value={inDate} onInput={handleInDate} />
+          <span> ~ </span>
+          <input class={tw`border`} for='date' value={outDate} onInput={handleOutDate} />
+        </div>
+        <div class={tw`flex justify-end mt-2`}><button onClick={handleAddCareer} class={tw`rounded p-1 pl-2 pr-2 bg-blue-400`}>추가</button></div>
+      </div>
+    </div>
   )
 }
