f1987770f90df65599554b48c56b68f22b192d95
tahott <tahott@gmail.com>
2023-01-05 23:39:21 +0900

    feat: Add dropdown position

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -31,13 +31,16 @@ export default function Navbar({ active, user, authUrl }: NavbarProps) {
         <ul class="flex justify-end gap-8 mx-4">
           {items.map((item) => <NavItem active={active} name={item.name} href={item.href} />)}
           <li>
-            <a href={authUrl ? authUrl : '/api/signOut'}>
-              {
-                user?.avatar_url ? (
+            {
+              user?.avatar_url ? (
+                <div class="relative inline-block">
                   <img src={user?.avatar_url} class="w-[24px] h-[24px] rounded" />
-                ) : <span>Sign In</span>
-              }
-            </a>
+                  <div class="absolute w-[100px] top-10 right-0 bg-gray-200">
+                    <a href="/api/signOut"><p class="px-2 py-1 text-indigo-500">sign out</p></a>
+                  </div>
+                </div>
+              ) : <a href={authUrl}><span>Sign In</span></a>
+            }
           </li>
         </ul>
       </div>
